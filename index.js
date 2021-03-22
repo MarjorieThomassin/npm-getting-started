1b2e0d896f658a21e4ae28de4e35fb645c3d223c
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2021-03-22 10:14:30 +0100

    cow

---
+++ index.js
@@ -0,0 +1,10 @@
+const myInfos = require('./information.js');
+myInfos();
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hello I'm Marjorie from Reims Campus !",
+    e : "o0",
+    T : "U"
+})); 
---
+++ information.js
@@ -0,0 +1,7 @@
+let me = ["Marjorie Thomassin", "WildCodeSchoolReims"];
+
+const myInfos = () => {
+    console.log(`Hi I am ${me}`);
+};
+
+module.exports = myInfos;
